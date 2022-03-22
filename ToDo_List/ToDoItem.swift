27e83bf01c1806d061cc46d405ac49245d85996d
Brennan Twiggs <btwiggs@zagmail.gonzaga.edu>
2022-03-21 21:31:18 -0700

    Completed 5.6

---
+++ ToDo_List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo_List
+//
+//  Created by Brennan Twiggs on 3/21/22.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes:String
+}
